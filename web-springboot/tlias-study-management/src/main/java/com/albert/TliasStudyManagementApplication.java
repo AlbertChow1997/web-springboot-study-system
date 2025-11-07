de1b6badbe5f3d62b13c52a00608a0c3798095f9
ZhouJW <525227990@qq.com>
2025-11-07 14:42:34 +0000

    build the project

---
+++ web-springboot/tlias-study-management/src/main/java/com/albert/TliasStudyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.albert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasStudyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasStudyManagementApplication.class, args);
+    }
+
+}
