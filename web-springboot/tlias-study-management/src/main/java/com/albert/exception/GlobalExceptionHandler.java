c9d2c7ba6888986f1c2134f670333d5eeeacbb58
ZhouJW <525227990@qq.com>
2026-01-27 15:10:42 +0000

    Add Global Exception Handler

---
+++ web-springboot/tlias-study-management/src/main/java/com/albert/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.albert.exception;
+
+
+import com.albert.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public Result handleException(Exception e){
+        log.error("handleException: ", e);
+        return Result.error("Error! Please connect the administrator");
+
+    }
+
+    //first handle the exact exception, then handle the parent exception
+    @ExceptionHandler
+    public Result DuplicateKeyException(DuplicateKeyException e){
+        log.error("DuplicateKeyException: ", e);
+        String msg = e.getMessage();
+        String errorMsg = msg.substring(msg.indexOf("Duplicate entry"));
+        String[] arr = errorMsg.split(" ");
+        return Result.error("Value " +arr[2] + " is exist");
+
+    }
+
+}
