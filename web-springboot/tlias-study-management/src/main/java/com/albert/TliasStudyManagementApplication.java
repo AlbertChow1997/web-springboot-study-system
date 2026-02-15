60a723954705103d23fc0a3401fb7c0688cb08ce
ZhouJW <525227990@qq.com>
2026-02-15 02:02:39 +0000

    add Filter component

--- web-springboot/tlias-study-management/src/main/java/com/albert/TliasStudyManagementApplication.java
+++ web-springboot/tlias-study-management/src/main/java/com/albert/TliasStudyManagementApplication.java
@@ -2,10 +2,12 @@ package com.albert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import java.util.Scanner;
 
 @SpringBootApplication
+@ServletComponentScan
 public class TliasStudyManagementApplication {
 
     public static void main(String[] args) {
---
+++ web-springboot/tlias-study-management/src/main/java/com/albert/filter/DemoFilter.java
@@ -0,0 +1,27 @@
+package com.albert.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(urlPatterns = "/*")
+public class DemoFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("DemoFilter doFilter");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("DemoFilter init");
+    }
+
+    @Override
+    public void destroy() {
+        log.info("DemoFilter destroy");
+    }
+}
