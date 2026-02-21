350dee62a01b4c1a62729660b37bd81edc1961eb
ZhouJW <525227990@qq.com>
2026-02-21 14:34:34 +0000

    add TokenInterceptor and WebConfig

--- web-springboot/tlias-study-management/src/main/java/com/albert/interceptor/TokenInterceptor.java
+++ web-springboot/tlias-study-management/src/main/java/com/albert/interceptor/TokenInterceptor.java
@@ -1,22 +1,18 @@
-package com.albert.filter;
+package com.albert.interceptor;
 
 import com.albert.utils.JwtUtils;
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 
 @Slf4j
-//@WebFilter(urlPatterns = "/*")
-public class TokenFilter implements Filter {
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         //1. Get request path
         String requestPath = request.getRequestURI();
@@ -24,8 +20,7 @@ public class TokenFilter implements Filter {
         //2. Judge if it is login request
         if (requestPath.contains("/login")){
             log.info("Login request, skip token filter");
-            filterChain.doFilter(request, response);
-            return;
+            return true;
         }
 
         //3. Get token from request header
@@ -35,7 +30,7 @@ public class TokenFilter implements Filter {
         if (token == null || token.isEmpty()){
             log.error("token is empty, return 401");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
+            return false;
         }
 
         //5. If token exist, judge if it is valid. If not, return error (401).
@@ -44,12 +39,11 @@ public class TokenFilter implements Filter {
         } catch (Exception e) {
             log.error("token is invalid, return 401");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
+            return false;
         }
 
         //6. If token is valid, continue to process the request.
         log.info("token is valid, continue to process the request");
-        filterChain.doFilter(request, response);
+        return true;
     }
-
 }
